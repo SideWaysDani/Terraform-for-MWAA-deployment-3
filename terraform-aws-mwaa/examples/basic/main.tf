8f1a0ff99a141cbcbc371d7f627b7dc7930e4d30
Muhammad Daniyal <88679856+SideWaysDani@users.noreply.github.com>
2024-04-24 00:30:53 +0500

    Update main.tf

--- terraform-aws-mwaa/examples/basic/main.tf
+++ terraform-aws-mwaa/examples/basic/main.tf
@@ -89,7 +89,7 @@ module "mwaa" {
   source = "../.."
 
   name              = var.name
-  airflow_version   = "2.2.2"
+  airflow_version   = "2.8.1"
   environment_class = "mw1.medium"
   create_s3_bucket  = false
   source_bucket_arn = aws_s3_bucket.this.arn
