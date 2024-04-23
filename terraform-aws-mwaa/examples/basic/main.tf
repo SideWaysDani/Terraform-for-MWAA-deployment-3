24beda78e6ca9f7900995eff3ee092ead4874bff
Muhammad Daniyal <88679856+SideWaysDani@users.noreply.github.com>
2024-04-24 01:10:01 +0500

    Update main.tf

--- terraform-aws-mwaa/examples/basic/main.tf
+++ terraform-aws-mwaa/examples/basic/main.tf
@@ -26,9 +26,6 @@ module "vpc" {
   name = var.name
   cidr = var.vpc_cidr
 
-  azs             = local.azs
-  public_subnets  = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 8, k)]
-  private_subnets = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 8, k + 10)]
 
   enable_nat_gateway   = true
   single_nat_gateway   = true
