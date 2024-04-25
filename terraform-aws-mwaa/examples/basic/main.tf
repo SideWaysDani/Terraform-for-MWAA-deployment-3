a3f964bd803ef354164c420cb697ee435e08e03c
Muhammad Daniyal <88679856+SideWaysDani@users.noreply.github.com>
2024-04-25 22:24:07 +0500

    Update main.tf
    Made version <3

--- terraform-aws-mwaa/examples/basic/main.tf
+++ terraform-aws-mwaa/examples/basic/main.tf
@@ -150,7 +150,7 @@ module "mwaa" {
 #---------------------------------------------------------------
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 3.0"
+  version = "< 3.0"
 
   name = var.name
   cidr = var.vpc_cidr
