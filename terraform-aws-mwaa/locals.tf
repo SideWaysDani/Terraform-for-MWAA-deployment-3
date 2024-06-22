a4d88200cd1b07b9a0b3c3b7a4217362340384ec
Muhammad Daniyal <88679856+SideWaysDani@users.noreply.github.com>
2024-06-22 20:14:56 +0500

    Update locals.tf to use existing role instead of creating new one

--- terraform-aws-mwaa/locals.tf
+++ terraform-aws-mwaa/locals.tf
@@ -1,5 +1,9 @@
 locals {
-  execution_role_arn = var.create_iam_role ? aws_iam_role.mwaa[0].arn : var.execution_role_arn
+  # Commented out the conditional statement for creating a new role
+  # execution_role_arn = var.create_iam_role ? aws_iam_role.mwaa[0].arn : var.execution_role_arn
+
+  # Directly set the execution role ARN to your existing role
+  execution_role_arn = "arn:aws:iam::953761584870:role/service-role/AmazonMWAA-MyAirflowEnvironment-shrn1T"
 
   security_group_ids = var.create_security_group ? concat([aws_security_group.mwaa[0].id], var.security_group_ids) : var.security_group_ids
 
